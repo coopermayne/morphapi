5b20bf8cd342881418e727eee1ee1c1634fc063f
Cooper Mayne <coopermayne@gmail.com>
2016-01-29 22:08:20 -0500

    added menu contrl

--- app/controllers/menu_controller.rb
+++ app/controllers/menu_controller.rb
@@ -1,4 +1,60 @@
 class MenuController < ApplicationController
   def index
+
+    res = [
+      {
+      title: 'Morphosis',
+      url: '',
+      sorting: [ {
+        title: 'Awards',
+        items: ''
+      }, {
+        title: 'People',
+        items: ''
+      }, {
+        title: 'Media',
+        items: ['Publications', 'Bibliography', 'Exhibitions', 'Video']
+      } ]
+    },
+
+    {
+      title: 'Architecture',
+      url: 'architecture',
+      sorting: [ {
+        title: 'Alphabetical',
+        items: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+      }, {
+        title: 'Year',
+        items: [ '1970-1979', '1980-1989', '1990-1999', '2000-2009', '2010-2019' ]
+      }, {
+        title: 'Type',
+        items: Project.all.select {|p| p.section && p.section.title=="Architecture"}.map {|p| p.project_types.first.title }.uniq
+      }, {
+        title: 'Location',
+        items: []
+      } ]
+    }, 
+    
+    {
+      title: 'Urban Design',
+      url: 'urban-design'
+    }, 
+    
+    {
+      title: 'Tangents',
+      url: 'tangents'
+    }, {
+      title: 'Research',
+      url: 'research'
+    }, {
+      title: 'Media',
+      url: 'media'
+    }, {
+      title: 'News',
+      url: 'news'
+    }]
+
+    render json: res
   end
+
 end
