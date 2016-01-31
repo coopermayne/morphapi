36a8f382bb4d58e7e0874b0912b367e81cb59c99
Cooper Mayne <coopermayne@gmail.com>
2016-01-31 18:14:52 -0500

    remove galleries

--- db/migrate/20160131191855_add_project_to_galleries.rb
+++
@@ -1,5 +0,0 @@
-class AddProjectToGalleries < ActiveRecord::Migration
-  def change
-    add_reference :galleries, :project, index: true, foreign_key: true
-  end
-end
--- db/migrate/20160131191943_add_gallery_to_uploads.rb
+++
@@ -1,5 +0,0 @@
-class AddGalleryToUploads < ActiveRecord::Migration
-  def change
-    add_reference :uploads, :gallery, index: true, foreign_key: true
-  end
-end
