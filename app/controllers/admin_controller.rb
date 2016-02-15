b7e1a5e4b1c30363dda2acae505595bad25006ab
Cooper Mayne <coopermayne@gmail.com>
2016-02-14 20:17:26 -0500

    route fixes

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,2 +1,12 @@
 class AdminController < ApplicationController
+  before_action :require_user
+
+  private
+
+  def require_user
+    unless user_signed_in?
+      flash[:error] = "You aren't authorized to manage content"
+      redirect_to new_user_session_path
+    end
+  end
 end
