43e3eb2f7f1fae6e2594c05d292eecf78f10fe42
Cooper Mayne <coopermayne@gmail.com>
2016-02-28 20:35:50 -0500

    slides

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -71,7 +71,7 @@ class Admin::UsersController < AdminController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:email, :password, :password_confirmation )
+      params.require(:user).permit(:name, :email, :password, :password_confirmation )
     end
 
     def require_admin
