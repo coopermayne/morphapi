cb2b4354be6b113fac4887ce0bd6e4ffec5202c0
Cooper Mayne <coopermayne@gmail.com>
2016-03-02 14:51:48 -0500

    includes uploads in media ctrl

--- app/controllers/media_controller.rb
+++ app/controllers/media_controller.rb
@@ -4,10 +4,10 @@ class MediaController < ApplicationController
   def index
 
     #publications #videos
-    media = Project.includes(:section, :project_types, :primary_image).select{|p| p.section && (p.section.title=="Publications" || p.section.title=="Videos"  )}
+    media = Project.includes(:uploads, :section, :project_types, :primary_image).select{|p| p.section && (p.section.title=="Publications" || p.section.title=="Videos"  )}
     
     #bibliography
-    bibs = BibliographyItem.includes(:primary_image)
+    bibs = BibliographyItem.includes(:uploads, :primary_image)
 
     @items = media + bibs
   end
