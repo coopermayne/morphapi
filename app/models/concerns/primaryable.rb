4d1b9a9f6a3c061280d833c1428a94623e62aa05
Cooper Mayne <coopermayne@gmail.com>
2016-04-21 15:48:06 -0700

    proper gallery sorting for API

--- app/models/concerns/primaryable.rb
+++ app/models/concerns/primaryable.rb
@@ -21,9 +21,9 @@ module Primaryable
   end
 
   def getGalleries
-    grouped = self.uploads.select{|u| u.in_gallery && u.is_image}.group_by{|item| item.file_type}
+    grouped = self.uploads.select{|u| u.in_gallery && u.is_image}.group_by{|item| item.file_type}.sort_by{|k,v| k.rank}
     res = {}
-    grouped.each{ |k, v| res[k.title.to_sym] = v.sort_by(&:rank) }
+    grouped.each{ |sorted| res[sorted[0].title.to_sym] = sorted[1].sort_by(&:rank) }
     res
   end
 
